610500c8c0f5f0b468943586f47ff29db4063ca3
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-04-13 00:44:23 +0200

    connected as an employee > new bill form displayed unit test

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -5,14 +5,34 @@
 import { screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
-
+import { localStorageMock } from "../__mocks__/localStorage.js"
+import { bills } from "../fixtures/bills.js"
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
+import router from "../app/Router.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+    test("Then the new bill page and its form should be displayed", () => {
+        Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+        window.localStorage.setItem('user', JSON.stringify({
+          type: 'Employee'
+        }))
+        // rooter render all the views into a root div by default
+        const root = document.createElement("div")
+        root.setAttribute("id", "root")
+        document.body.append(root)
+        // define window.onNavigate : app/router.js / onNavigate +-= window.history.pushState()
+        router()
+        // pushing billsUI into the vDOM
+        window.onNavigate(ROUTES_PATH.NewBill)
+        expect(screen.getByText('Envoyer une note de frais')).toBeTruthy()
+        expect(screen.getByText('Type de dépense')).toBeTruthy()
+        expect(screen.getByText('Nom de la dépense')).toBeTruthy()
+        expect(screen.getByText('Date')).toBeTruthy()
+        expect(screen.getByText('Montant TTC')).toBeTruthy()
+        expect(screen.getByText('TVA')).toBeTruthy()
+        expect(screen.getByText('Commentaire')).toBeTruthy()
+        expect(screen.getByText('Justificatif')).toBeTruthy()
     })
   })
 })
