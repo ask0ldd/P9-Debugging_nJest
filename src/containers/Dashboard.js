c1c6799a0b1257f1c85b00be3ebada2108e8fd20
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-06-09 15:19:02 +0200

    some comments > out

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -90,11 +90,6 @@ export default class {
   }
 
   handleEditTicket(e, bill, bills) {
-    /* TRASHED ORIGINAL CODE :
-    if (this.counter === undefined || this.id !== bill.id) this.counter = 0
-    if (this.id === undefined || this.id !== bill.id) this.id = bill.id
-    if (this.counter % 2 === 0) {*/
-
     // tracking the active bill is enough to achieve an even better result
     if(this.activeBill.id !== bill.id)
     {
@@ -144,21 +139,6 @@ export default class {
 
 
   handleShowTickets(e, bills, index) { // index = index of the category to open / close
-    /* TRASHED ORIGINAL CODE :
-    if (this.counter === undefined || this.index !== index) this.counter = 0
-    if (this.index === undefined || this.index !== index) this.index = index
-    if (this.counter % 2 === 0) {
-      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
-      $(`#status-bills-container${this.index}`)
-        .html(cards(filteredBills(bills, getStatus(this.index))))
-      this.counter ++
-    } else {
-      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
-      $(`#status-bills-container${this.index}`)
-        .html("")
-      this.counter ++
-    }*/
-
     // [Bug Hunt] - Dashboard tickets : triggered by open tickets lists arrow
     // check if the category is opened through innerHTML, so no need of any tracking variable
     if($(`#status-bills-container${index}`)?.html().trim() === ""){
